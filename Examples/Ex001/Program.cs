14db748adc0bfc659533a6683dd07059c7a9acb7
dpolitov <dannn83@mail.ru>
2022-12-02 09:33:23 +0300

    correction Exx001

--- Examples/Ex001/Program.cs
+++ Examples/Ex001/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.Write ("Введите первое число=");
+string number1 = Console.ReadLine();
+Console.Write ("Введите второе число=");
+string number2 = Console.ReadLine();
+int c= int.Parse(number1);
+int d= int.Parse(number2);
+if (c>d)
+{
+    Console.WriteLine($"Максимальное число {c}\n");
+}
+else
+{
+    Console.WriteLine($"Максимальное число {d}\n");
+}

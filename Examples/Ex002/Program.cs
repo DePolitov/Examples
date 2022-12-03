75858e0c44e32ddde82cc2ac5fbf3475234e272a
dpolitov <dannn83@mail.ru>
2022-12-03 10:48:47 +0300

    add exx002,3,4

---
+++ Examples/Ex002/Program.cs
@@ -0,0 +1,20 @@
+﻿int a;
+int b;
+int c;
+int max;
+Console.Write ("Введите первое число -");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write ("Введите второе число -");
+int.TryParse(Console.Readline()!, out b);
+Console.Write ("Введите третье число -");
+int.TryParse(Console.Readline()!, out c);
+if (b>a)
+{
+    int max=b;
+}
+if (c>a)
+{
+   int max=c;
+}
+else {int max=a;}
+Console.Write($"Максимальное число - {max}\n");
---
+++ Examples/Exx003/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+int a;
+Console.Write("Введите число - ");
+int.TryParse(Console.ReadLine()!, out a);
+if (a % 7 == 0 && a % 23 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}

36d9b29ab575cb285e79313e13a63986e8a724f0
dpolitov <dannn83@mail.ru>
2022-12-03 10:48:04 +0300

    add Exx2,3,4

---
+++ Examples/Ex004/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a,b;
+Console.Write("Введите число 1 - ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write("Введите число 2 - ");
+int.TryParse(Console.ReadLine()!, out b);
+if (a/b==b)
+{
+    Console.WriteLine ("Число 1 является квадратом 2го");
+}
+else{
+    if (b/a==a)
+    {
+      Console.WriteLine ("Число 2 является квадратом 1го");  
+    }
+    else{
+        Console.WriteLine ("нет");
+    }
+
+}
